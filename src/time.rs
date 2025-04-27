cd65dd4530b831bca0e461798a8891f1f7947a98
itscrystalline <pvpthadgaming@gmail.com>
2025-04-27 17:46:44 +0700

    test: added test for weekdays

--- src/time.rs
+++ src/time.rs
@@ -26,3 +26,35 @@ impl TimeRange {
         match_year && match_month && match_day
     }
 }
+
+#[cfg(test)]
+mod test {
+    use map_macro::hash_set;
+
+    use crate::config::DayOf;
+    use chrono::{Local, Month, TimeZone, Weekday};
+
+    use super::*;
+
+    fn date(year: i32, month: u32, day: u32) -> DateTime<FixedOffset> {
+        Local
+            .with_ymd_and_hms(year, month, day, 0, 0, 0)
+            .unwrap()
+            .fixed_offset()
+    }
+
+    #[test]
+    fn eval_datetime_days_of_week() {
+        let time = TimeRange {
+            day_of: Some(DayOf::Week(hash_set! {Weekday::Mon, Weekday::Fri})),
+            month: None,
+            year: None,
+        };
+        let monday = date(2025, 4, 28);
+        let friday = date(2025, 5, 2);
+        let next_week = date(2025, 5, 5);
+        assert!(time.eval_with_datetime(monday));
+        assert!(time.eval_with_datetime(friday));
+        assert!(time.eval_with_datetime(next_week));
+    }
+}
