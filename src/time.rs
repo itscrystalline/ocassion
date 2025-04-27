8f291e6f8cf2a1256cf1b331502e07fb5c379ea4
itscrystalline <pvpthadgaming@gmail.com>
2025-04-27 21:06:24 +0700

    test: added tests for the added functions

--- src/time.rs
+++ src/time.rs
@@ -71,7 +71,7 @@ mod test {
     use map_macro::hash_set;
 
     use crate::config::DayOf;
-    use chrono::{Local, TimeZone, Weekday};
+    use chrono::{Local, Month, TimeZone, Weekday};
 
     use super::*;
 
@@ -83,17 +83,126 @@ mod test {
     }
 
     #[test]
+    fn eval_now() {
+        let now = Local::now().fixed_offset();
+        let time = TimeRange {
+            day_of: Some(DayOf::Month(hash_set! { now.day() as u8 })),
+            month: Some(hash_set! { Month::try_from(now.month() as u8).unwrap() }),
+            year: Some(hash_set! { now.year() }),
+        };
+        assert!(time.evaluate());
+    }
+
+    #[test]
     fn eval_datetime_days_of_week() {
         let time = TimeRange {
-            day_of: Some(DayOf::Week(hash_set! {Weekday::Mon, Weekday::Fri})),
+            day_of: Some(DayOf::Week(hash_set! { Weekday::Mon, Weekday::Fri })),
             month: None,
             year: None,
         };
         let monday = date(2025, 4, 28);
         let friday = date(2025, 5, 2);
+        let sunday = date(2025, 5, 4);
         let next_week = date(2025, 5, 5);
         assert!(time.eval_with_datetime(monday));
         assert!(time.eval_with_datetime(friday));
+        assert!(!time.eval_with_datetime(sunday));
         assert!(time.eval_with_datetime(next_week));
+    }
+    #[test]
+    fn eval_datetime_days_of_month() {
+        let time = TimeRange {
+            day_of: Some(DayOf::Month(hash_set! { 1, 2, 5 })),
+            month: None,
+            year: None,
+        };
+        let first = date(2020, 1, 1);
+        let second = date(2027, 3, 2);
+        let third = date(2012, 12, 3);
+        let fifth = date(2021, 9, 5);
+        assert!(!time.eval_with_datetime(third));
+        assert!(time.eval_with_datetime(first));
+        assert!(time.eval_with_datetime(second));
+        assert!(time.eval_with_datetime(fifth));
+    }
+    #[test]
+    fn eval_datetime_month() {
+        let time = TimeRange {
+            day_of: None,
+            month: Some(hash_set! { Month::January, Month::March, Month::September }),
+            year: None,
+        };
+        let jan = date(2020, 1, 1);
+        let march = date(2027, 3, 2);
+        let april = date(2012, 4, 3);
+        let sep = date(2021, 9, 5);
+        assert!(!time.eval_with_datetime(april));
+        assert!(time.eval_with_datetime(jan));
+        assert!(time.eval_with_datetime(march));
+        assert!(time.eval_with_datetime(sep));
+    }
+    #[test]
+    fn eval_datetime_year() {
+        let time = TimeRange {
+            day_of: None,
+            month: None,
+            year: Some(hash_set! { 2022, 2023, 2025 }),
+        };
+        let year22 = date(2022, 1, 1);
+        let year23 = date(2023, 3, 2);
+        let year24 = date(2024, 4, 3);
+        let year25 = date(2025, 9, 5);
+        assert!(!time.eval_with_datetime(year24));
+        assert!(time.eval_with_datetime(year22));
+        assert!(time.eval_with_datetime(year23));
+        assert!(time.eval_with_datetime(year25));
+    }
+
+    #[test]
+    fn message_now() {
+        let now = Local::now().fixed_offset();
+        let range = TimeRangeMessage {
+            message: "hewwo !".to_string(),
+            time: TimeRange {
+                day_of: Some(DayOf::Month(hash_set! { now.day() as u8 })),
+                month: None,
+                year: None,
+            },
+        };
+        let range_tmrw = TimeRangeMessage {
+            message: "hewwo !".to_string(),
+            time: TimeRange {
+                day_of: Some(DayOf::Month(hash_set! { now.day() as u8 + 1 })),
+                month: None,
+                year: None,
+            },
+        };
+        let result = range.try_message();
+        assert_eq!(result.unwrap(), "hewwo !");
+        let result = range_tmrw.try_message();
+        assert!(result.is_none());
+    }
+    #[test]
+    fn message() {
+        let range = TimeRangeMessage {
+            message: "hewwo !".to_string(),
+            time: TimeRange {
+                day_of: Some(DayOf::Month(hash_set! { 3, 5, 9 })),
+                month: Some(hash_set! { Month::June }),
+                year: None,
+            },
+        };
+
+        let first_june = date(2025, 6, 1);
+        let third_june = date(2025, 6, 3);
+        let fifth_june = date(2025, 6, 5);
+        let ninth_june = date(2025, 6, 9);
+        let third_july = date(2025, 7, 3);
+
+        assert!(range.try_with_datetime(first_june).is_none());
+        assert!(range.try_with_datetime(third_july).is_none());
+        assert_eq!(range.try_with_datetime(third_june).unwrap(), "hewwo !");
+        assert_eq!(range.try_with_datetime(fifth_june).unwrap(), "hewwo !");
+        assert_eq!(range.try_with_datetime(ninth_june).unwrap(), "hewwo !");
     }
 }
