6b88b55e757d669208a7a11f4fb3ac09cc5f4f88
itscrystalline <pvpthadgaming@gmail.com>
2025-04-27 21:29:12 +0700

    feat: main program

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use std::io::ErrorKind;
+
+use ocassion::{config::Config, errors::ConfigError};
+
+fn main() {
+    let config = match Config::load() {
+        Ok(conf) => conf,
+        Err(ConfigError::Io(err)) if err.kind() == ErrorKind::NotFound => {
+            match Config::save_default() {
+                Ok(()) => match Config::load() {
+                    Ok(conf) => conf,
+                    Err(_) => return,
+                },
+                Err(_) => return,
+            }
+        }
+        _ => return,
+    };
+    for message in config.dates {
+        let Some(msg) = message.try_message() else {
+            continue;
+        };
+        print!("{msg}");
+    }
+}
