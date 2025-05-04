2876e160fec725e247339b98d7d358562a3db375
itscrystalline <pvpthadgaming@gmail.com>
2025-05-04 14:17:29 +0700

    feat: add more errors

--- src/errors.rs
+++ src/errors.rs
@@ -4,10 +4,14 @@ use thiserror::Error;
 pub enum ConfigError {
     #[error("this error should never fire")]
     Unknown,
+    #[error("the path in $OCCASION_CONFIG or one of it's imports is not a file path")]
+    NotAFile,
     #[error("malformed config file")]
     Malformed,
     #[error("cannot determine config dir, pass $OCCASION_CONFIG directly")]
     UndeterminableConfigLocation,
+    #[error("max import depth reached (3)")]
+    MaxRecursionDepth,
     #[error("i/o error: {0}")]
     Io(#[from] std::io::Error),
     #[error("cannot parse: {0}")]
