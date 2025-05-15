3c3b5a6e9f6203d3620a9ca987e522ed6075ba57
itscrystalline <pvpthadgaming@gmail.com>
2025-05-15 18:18:01 +0700

    test: add tests for tokenizer and string parsing

--- src/formatting.rs
+++ src/formatting.rs
@@ -79,3 +79,114 @@ impl TryFrom<String> for FormatString {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod unit_tests {
+    use colored::Styles;
+
+    use super::*;
+
+    #[test]
+    fn test_tokenizer() {
+        let test = "hi :3 (this is part of a format string (and another)[underline] )[bold] and this is not".to_string();
+        let result = Parser::tokenize(test);
+        assert_eq!(
+            result.tokens,
+            [
+                Token::Literal("hi :3 ".to_string()),
+                Token::OpenContent,
+                Token::Literal("this is part of a format string ".to_string()),
+                Token::OpenContent,
+                Token::Literal("and another".to_string()),
+                Token::CloseContent,
+                Token::OpenStyle,
+                Token::Literal("underline".to_string()),
+                Token::CloseStyle,
+                Token::Literal(" ".to_string()),
+                Token::CloseContent,
+                Token::OpenStyle,
+                Token::Literal("bold".to_string()),
+                Token::CloseStyle,
+                Token::Literal(" and this is not".to_string())
+            ]
+        )
+    }
+    #[test]
+    fn test_tokenizer_escaped() {
+        let test = "hi :3 (this is part of a format string (and another)[underline] )[bold] and this is not \\(this is escaped\\)".to_string();
+        let result = tokenize(test);
+        assert_eq!(
+            result,
+            [
+                Token::Literal("hi :3 ".to_string()),
+                Token::OpenContent,
+                Token::Literal("this is part of a format string ".to_string()),
+                Token::OpenContent,
+                Token::Literal("and another".to_string()),
+                Token::CloseContent,
+                Token::OpenStyle,
+                Token::Literal("underline".to_string()),
+                Token::CloseStyle,
+                Token::Literal(" ".to_string()),
+                Token::CloseContent,
+                Token::OpenStyle,
+                Token::Literal("bold".to_string()),
+                Token::CloseStyle,
+                Token::Literal(" and this is not (this is escaped)".to_string())
+            ]
+        )
+    }
+
+    #[test]
+    fn basic_from_string() {
+        let test = "hi guys".to_string();
+        let test_struct = FormatString {
+            nodes: vec![FormatStringNode::String("hi guys".to_string())],
+        };
+        let formatted = FormatString::try_from(test);
+        assert_eq!(formatted.unwrap(), test_struct);
+    }
+    #[test]
+    fn basic_from_formatted_string() {
+        let test = "hi guys (this one is green)[fg:green] i am not green".to_string();
+        let test_struct = FormatString {
+            nodes: vec![
+                FormatStringNode::String("hi guys ".to_string()),
+                FormatStringNode::Formatted(FormatNode {
+                    children: vec![FormatStringNode::String("this one is green".to_string())],
+                    fg: Some(Color::Green),
+                    ..Default::default()
+                }),
+                FormatStringNode::String(" i am not green".to_string()),
+            ],
+        };
+        let formatted = FormatString::try_from(test);
+        assert_eq!(formatted.unwrap(), test_struct);
+    }
+    #[test]
+    fn basic_from_formatted_nested_string() {
+        let test = "hi (guys)[underline] (this one is green( and this one is also underlined and bold!)[underline bold])[fg:green] i am not green".to_string();
+        let test_struct = FormatString {
+            nodes: vec![
+                FormatStringNode::String("hi guys ".to_string()),
+                FormatStringNode::Formatted(FormatNode {
+                    children: vec![
+                        FormatStringNode::String("this one is green".to_string()),
+                        FormatStringNode::Formatted(FormatNode {
+                            children: vec![FormatStringNode::String(
+                                " and this one is also underlined and bold!".to_string(),
+                            )],
+                            style: Style::from_iter([Styles::Underline, Styles::Bold]),
+                            ..Default::default()
+                        }),
+                    ],
+                    fg: Some(Color::Green),
+                    ..Default::default()
+                }),
+                FormatStringNode::String(" i am not green".to_string()),
+            ],
+        };
+        let formatted = FormatString::try_from(test);
+        assert_eq!(formatted.unwrap(), test_struct);
+    }
+}
